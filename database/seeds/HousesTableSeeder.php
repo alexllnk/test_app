e045503f6cf955baa650f80287cceb224bbd743b
alex n1ke <st3am.pwr@gmail.com>
2020-03-06 23:25:43 +0200

    added a seeder for the House model with the .csv-file data import

---
+++ database/seeds/HousesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use JeroenZwart\CsvSeeder\CsvSeeder;
+
+class HousesTableSeeder extends CsvSeeder
+{
+    public function __construct()
+    {
+        $this->file = '/database/seeds/csvs/property-data.csv';
+        $this->tablename = 'houses';
+        $this->delimiter = ',';
+    }
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Recommended when importing larger CSVs
+        DB::disableQueryLog();
+        parent::run();
+    }
+}
